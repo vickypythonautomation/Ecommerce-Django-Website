05f3e081a6a033d1d3746c860ecdaabe3ebf6bfa
Vicky Kumar <imvickykumar999@gmail.com>
2026-03-01 14:45:45 +0530

    static folder working

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static/']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
