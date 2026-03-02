ebc1ad0b2a7f13d397f0e2c8abd12c9235ddd941
Vicky Kumar <imvickykumar999@gmail.com>
2026-03-02 13:42:05 +0530

    Login and Logout - Django Wednesdays ECommerce #7

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     products = Product.objects.all()
@@ -7,3 +9,9 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def login_user(request):
+    return render(request, 'login.html', {})
+
+def logout_user(request):
+    pass
